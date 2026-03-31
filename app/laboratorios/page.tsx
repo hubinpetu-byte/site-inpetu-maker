2e3b6ad2dd954a3b27e717280a6afba39463affa
InPETU hub <hub.inpetu@gmail.com>
2026-03-31 10:42:19 -0300

    fix: forçando atualização de módulos

--- app/laboratorios/page.tsx
+++ app/laboratorios/page.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 export default function LaboratoriosPage() {
   return (
-    <main className="p-20 text-center">
-      <h1 className="text-4xl font-black">Laboratórios InPETU</h1>
-      <p>Página em manutenção para ajustes técnicos.</p>
+    <main>
+      <h1>Laboratórios InPETU</h1>
     </main>
   );
 }
